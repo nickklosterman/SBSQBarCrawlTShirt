feb76073d2438839af7a28a199498b4f46b1af51
Nicolae Ceausescu <nicolae@nicolae-desktop.(none)>
2011-04-09 23:38:54 -0400

    added txt file lists

--- AddCommitPushSBSP.sh
+++ AddCommitPushSBSP.sh
@@ -4,8 +4,12 @@ then
     echo "usage: scriptname \"comment to commit\""
 else
     message=\"${1}\"
-#echo ${message}
-    git add .
-    git commit -a -m ${message}
-    git push git@github.com:nickklosterman/SBSQBarCrawlTShirt.git master
+echo ${message}
+echo "0"
+    git add . ;
+echo "1"
+    git commit -a -m ${message};
+echo "2"
+    git push git@github.com:nickklosterman/SBSQBarCrawlTShirt.git master;
+echo "3"
 fi
